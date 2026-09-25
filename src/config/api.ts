5bbd37b907d1323d116802e1dd2fb95d33f8cd76
ooctra <dima.klimo2014@gmail.com>
2026-09-25 19:11:03 +0300

    Fix API URL format

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,3 @@
 // src/config/api.ts
 
-const DEV_IP = '172.20.10.4';
-// const DEV_IP = '10.185.228.148';
-const DEV_PORT = '3001';
-
-export const BASE_URL = `http://${DEV_IP}:${DEV_PORT}`;
+export const BASE_URL = 'https://mypethealthapp.onrender.com';
