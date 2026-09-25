d8c24b33f0cd8ebf54c6ba4ff6df3a74eb121f88
ooctra <dima.klimo2014@gmail.com>
2026-09-25 18:53:55 +0300

    Fix API URL for Render backend

--- MyPetHealthApp/src/config/api.ts
+++ MyPetHealthApp/src/config/api.ts
@@ -1,6 +1,3 @@
 // src/config/api.ts
 
-const DEV_IP = '192.168.0.17';
-const DEV_PORT = '3001';
-
-export const BASE_URL = `http://${DEV_IP}:${DEV_PORT}`;
+export const BASE_URL = 'https://mypethealthapp.onrender.com';
